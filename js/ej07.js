4376b8068922cf1fbef0ca05f1fb9c8cbef4e97d
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-19 17:13:23 -0300

    feat: se agragea la resolucion del ejercicio 7

---
+++ js/ej07.js
@@ -0,0 +1,13 @@
+/*Enunciado
+Dado un array de frutas, mostrá cada una en consola usando un bucle for...of .
+
+Salida esperada
+manzana
+banana
+naranja
+pera*/
+
+const frutas = ['manzana', 'banana', 'naranja', 'pera']
+for (const fruta of frutas) {
+    console.log(fruta);
+}
