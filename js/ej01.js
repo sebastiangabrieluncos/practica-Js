41c7cc31ca7fb95132b0fb02ca0f9ed6d203fb09
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-19 13:37:37 -0300

    feat: se agrega la resolucion del ejercicio 1

---
+++ js/ej01.js
@@ -0,0 +1,14 @@
+/* Enunciado
+Declará una variable nombre con tu nombre y una constante edad con tu edad.
+Mostrá ambos valores en la consola.
+
+Salida esperada
+Lucas
+25
+*/
+
+let nombre ='Sebastián Gabriel Uncos';
+const edad = 36;
+
+console.log(nombre);
+console.log(edad)
