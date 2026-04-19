8f7d94f5222c231af53a6a66d21a7ec3ccbddc6d
sebastiangabrieluncos <sebastiangabrieluncos@gmail.com>
2026-04-19 14:00:24 -0300

    feat: se agrega la resolucion del ejercicio 3

---
+++ js/ej03.js
@@ -0,0 +1,11 @@
+/*Enunciado
+Dadas las variables nombre y edad , mostrá un mensaje usando template strings
+(comillas invertidas):
+Hola, me llamo Ana y tengo 30 años.
+*/
+
+let nombre = 'Aluminé';
+const edad = 12;
+
+console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`); 
+console.log( `${nombre}`) 
